c026470ef6b761b0375b44f57d9cd5bf249ed92f
Abdelrahman Gaber <141575652+abdelrahman365g@users.noreply.github.com>
2025-12-16 21:43:42 +0200

    Add files via upload

---
+++ input/test.java
@@ -0,0 +1,73 @@
+class TestClass {
+    function func(int x) {
+        int x = 10 + 2 * (3 - 1);
+        string y;
+        bool z;
+        x = 5 / 2 ;
+        if (x < 10) {
+            y = "hello";
+        } else {
+            return 20 - 5;
+        }
+        while (y >= 0) {
+            int z = 3;
+            z = z + 4;
+        }
+        for (int i = 0; i <= 5; i = i + 1) {
+            return i * 2;
+        }
+    }
+    function func() {
+        int x = 10 + 2 * (3 - 1);
+        string y;
+        bool z;
+        if (x < 10) {
+            y = "hello";
+        } else {
+            return 20 - 5;
+        }
+        while (y >= 0) {
+            int z = 3;
+            z = z + 4;
+        }
+        for (int i = 0; i <= 5; i = i + 1) {
+            return i * 2;
+        }
+    }
+}
+class TestClass {
+    function func(string y) {
+        int x = 10 + 2 * (3 - 1);
+        string y;
+        bool z;
+        if (x < 10) {
+            y = "hello";
+        } else {
+            return 20 - 5;
+        }
+        while (y >= 0) {
+            int z = 3;
+            z = z + 4;
+        }
+        for (int i = 0; i <= 5; i = i + 1) {
+            return i * 2;
+        }
+    }
+    function func(bool z) {
+        int x = 10 + 2 * (3 - 1);
+        string y;
+        bool z;
+        if (x < 10) {
+            y = "hello";
+        } else {
+            return 20 - 5;
+        }
+        while (y >= 0) {
+            int z = 3;
+            z = z + 4;
+        }
+        for (int i = 0; i <= 5; i = i + 1) {
+            return i * 2;
+        }
+    }
+}
